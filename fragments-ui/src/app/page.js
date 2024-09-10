4764eb2a388c4907d4f46cd045f0a24b9763ae0d
mrahman141 <mrahman141@myseneca.ca>
2024-09-10 18:45:45 -0400

    Added Cognito
    aws-amplify configured, user can login and logout via the home page, the id token and access token can be decoded via JWT and gives proper information about user data

--- fragments-ui/src/app/page.js
+++ fragments-ui/src/app/page.js
@@ -1,25 +1,77 @@
+'use client'
+import {Auth, getUser} from '../auth'
 // Button is used from Shadcn/ui from https://ui.shadcn.com/docs/components/button
 import { Button } from "../components/ui/button"
+import React, { useState, useEffect } from 'react';
 import Link from "next/link"
 
 
 export default function Home() {
+
+  const [username, setUsername] = useState('');
+  const [LoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = async (e) => {
+    try {
+      await Auth.federatedSignIn();
+    } catch (error) {
+      console.error('Failed to sign in:', error);
+    }
+  }
+  
+  const handleLogout = async (e) => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.error('Failed to sign out:', error);
+    }
+  }
+
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const user = await getUser();
+        if (!user) {
+          setLoggedIn(false)
+        }
+        else{
+          console.log(user);
+          setUsername(user.username);
+          setLoggedIn(true)
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    // Call the fetchUserData function
+    fetchUserData();
+
+
+  }, [LoggedIn]);
+
   return (
     <div>
       <h1 className="text-5xl text-center pt-10 text-white">Welcome to Fragments-ui</h1>
 
-      <h1 className="text-3xl text-center pt-10 text-white">Hello Username (Only show if logged in)</h1>
+      {LoggedIn == true ? (
+        <h1 className="text-3xl text-center pt-10 text-white">Hello, {username}</h1>
+      ) : (
+        <></>
+      )}
 
       <div className="flex justify-center pt-7">
 
-        {/* Hide the div login if logged in and vice versa for logout */}
-        <div className="pr-2">
-          <Button variant="outline">Login</Button>
-        </div>
-
-        <div className="pl-2">
-          <Button variant="destructive">Logout</Button>
-        </div>
+        {LoggedIn == false ? (
+          <div className="pr-2">
+            <Button variant="outline" onClick={handleLogin}>Login</Button>
+          </div>
+        ) : (
+          <div className="pl-2">
+            <Button variant="destructive" onClick={handleLogout}>Logout</Button>
+          </div>
+        )}
 
       </div>
 
